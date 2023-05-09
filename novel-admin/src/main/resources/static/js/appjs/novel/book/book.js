b937e6bbc6c54eba2d4fb3a4f260549f5526a99d
zhuyebai <zhuybai@163.com>
2023-05-09 18:12:20 +0800

    手机端小说不显示问题

--- novel-admin/src/main/resources/static/js/appjs/novel/book/book.js
+++ novel-admin/src/main/resources/static/js/appjs/novel/book/book.js
@@ -143,10 +143,16 @@ function load() {
                         field: 'id',
                         align: 'center',
                         formatter: function (value, row, index) {
-                            var r = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
+                            var e = '<a  class="btn btn-primary btn-sm ' + s_edit_h + '" href="#" mce_href="#" title="编辑" onclick="edit(\''
+                                + row.id
+                                + '\')"><i class="fa fa-edit "></i></a> ';
+                            var d = '<a class="btn btn-warning btn-sm ' + s_remove_h + '" href="#" title="删除"  mce_href="#" onclick="remove(\''
                                 + row.id
                                 + '\')"><i class="fa fa-remove"></i></a> ';
-                            return r;
+                            var f = '<a class="btn btn-success btn-sm ' + s_detail_h + '" href="#" title="详情"  mce_href="#" onclick="detail(\''
+                                + row.id
+                                + '\')"><i class="fa fa-key"></i></a> ';
+                            return e + f + d;
                         }
                     }
 
